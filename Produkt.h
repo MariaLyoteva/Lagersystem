f5117687fc2d8b492c6d448d311ec01f82197a4a
MariaLyoteva <71443789+MariaLyoteva@users.noreply.github.com>
2023-07-21 17:20:16 +0300

    Create Produkt.h

---
+++ Produkt.h
@@ -0,0 +1,35 @@
+#ifndef PRODUKT_H
+#define PRODUKT_H
+
+#include <iostream>
+
+#include <string>
+
+class Produkt {
+public: Produkt(int id,const std::string & titel): produktId(id),titel(titel) {}
+
+    int getProduktId() const {
+        return produktId;
+    }
+
+    std::string getTitel() const {
+        return titel;
+    }
+
+    void setTitel(const std::string & newTitel) const {
+        titel = newTitel;
+    }
+
+    virtual std::string toString() const = 0;
+
+    friend std::ostream & operator << (std::ostream & os,
+                                       const Produkt & produkt);
+
+protected: int produktId;
+    std::string titel;
+};
+
+std::ostream & operator << (std::ostream & os,
+                            const Produkt & produkt);
+
+#endif // PRODUKT_H
