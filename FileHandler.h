9d4bd7814cce1a9cc360babe6f34546757d95130
MariaLyoteva <71443789+MariaLyoteva@users.noreply.github.com>
2024-03-16 12:03:47 +0200

    Update FileHandler.h

--- FileHandler.h
+++ FileHandler.h
@@ -1,5 +1,4 @@
-#ifndef FILEHANDLER_H
-#define FILEHANDLER_H
+#pragma once
 
 #include "Lagersystem.h"
 #include <fstream>
@@ -17,13 +16,23 @@
 
 
 class FileHandler {
-private: Lagersystem & lagersystem;
-    std::vector < std::string > errorMessages; //a vector that contains all the error messages, so they can be exported at he end in the console
-public: FileHandler(Lagersystem & lagersystem);
-    void readFromFile(const std::string & filename);
-    void exportToFile(const std::string & filename) const;
-    void replaceUnderscoresWithSpaces(std::string & str) const;
-    void printErrorMessages() const; //prints all the error messages
+public:
+    FileHandler(std::shared_ptr<Lagersystem> lagersystem);
+
+    // Reads the input from the file
+    void readFromFile(const std::string &filename);
+
+    // Exports data to a file
+    void exportToFile(const std::string &filename) const;
+
+    // Prints any error messages
+    void printErrorMessages() const;
+
+private:
+    std::shared_ptr<Lagersystem> lagersystem;
+    std::vector<std::string> errorMessages;
+
+    void replaceUnderscoresWithSpaces(std::string &str) const;
 };
 
-#endif // FILEHANDLER_H
+
