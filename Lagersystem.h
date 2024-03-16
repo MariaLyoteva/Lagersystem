103ee78408312bbf983c7b777062966378476758
MariaLyoteva <71443789+MariaLyoteva@users.noreply.github.com>
2024-03-16 12:06:59 +0200

    Update Lagersystem.h

--- Lagersystem.h
+++ Lagersystem.h
@@ -1,30 +1,32 @@
-#include"Produkt.h"
-#include"Kunde.h"
-#include "Lager.h"
+
+#include <iostream>
 #include <map>
+#include <memory>
+#include <string>
+#include "Kunde.h"
+#include "Produkt.h"
+#include "Lager.h"
 #include"IdAlreadyExistsException.h"
 #include"IdNotFoundLager.h"
 
+#pragma once
 
-#ifndef LAGERSYSTEM_H
-#define LAGERSYSTEM_H
-//a class that represents the sytem
 class Lagersystem {
 public:
-    std::map < int,Kunde > customers; // Map customerID to Kunde
-    std::map < int,Produkt * > produktkatalog; // Map productID to Produkt*
-    std::map < int,Lager > lagerbestand; // Map productID to Lager
+    void addCustomer(const std::shared_ptr<Kunde> &customer); //adds customers to the map
+    void addProdukt(const std::shared_ptr<Produkt> &produkt); //adds product to the catalog
+    void addLagerbestand(int productId, const Lager &stockLevel); //adds the "stock levels"
 
-    void addCustomer(const Kunde & customer); //adds customer to the map
-    void addProdukt(Produkt * produkt); //adds product to the map
-    void addLagerbestand(int productId,
-                         const Lager & stockLevel); //adds a "stocklevel" to the map
     void printProduktkatalog() const; //prints the catalog
-    void printLagerbestand() const; //prints the storage status
+    void printLagerbestand() const;  //prints the "stock levels"
     void printKunde() const; //prints the clients
-    std::string getAllAsString() const; // a function that turns everything into one big string. It's used in the exportFile function (FileHandlder.h)
-    void replaceSpacesWithUnderscores(std::string & str) const; // thid function replaces back the spaces with underscores
 
+    std::string getAllAsString() const;
+
+    void replaceSpacesWithUnderscores(std::string &str) const;
+
+private:
+    std::map<int, std::shared_ptr<Kunde>> customers; //A map that stores the customers'id as key and a shared_ptr<Kunde> as value
+    std::map<int, std::shared_ptr<Produkt>> produktkatalog; //A map that stores the product_id as key and a shared_ptr<Produkt> as value
+    std::map<int, Lager> lagerbestand; // A map that stores the product_id as key and object from the class Lager as value
 };
-
-#endif
